1b0797679c9ec6e5fa285b9ac93cba41af1f6bf8
Saif ali khan <141383317+Saifalikhan19196@users.noreply.github.com>
2024-10-16 14:54:53 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-﻿// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
@@ -12,30 +12,31 @@ export default function Header() {
 
     return (
         <header className="sticky top-0 z-50 shadow">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-3">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
-                        <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12 transition-transform duration-200 hover:scale-105" // Added hover effect
-                            alt="Logo"
-                        />
+                        {/* Updated logo with "Portfolio" in half gray and half orange */}
+                        <span className="text-3xl font-bold transition-transform duration-200 hover:scale-105">
+                            <span className="text-gray-800">Port</span>
+                            <span className="text-orange-600">folio</span>
+                        </span>
                     </Link>
                     <div className="flex items-center lg:order-2 ">
                         {/* Always show Log in and Get Started buttons */}
                         <div className="hidden lg:block ">
-                        <NavLink
-                            to="/login"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
-                        >
-                            Log in
-                        </NavLink>
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
-                        >
-                            Get started
-                        </Link>  </div>
+                            <NavLink
+                                to="/login"
+                                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
+                            >
+                                Log in
+                            </NavLink>
+                            <Link
+                                to="#"
+                                className="text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
+                            >
+                                Get started
+                            </Link>
+                        </div>
                         {/* Menu toggle button for mobile */}
                         <button
                             className="inline-flex p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
@@ -57,8 +58,8 @@ export default function Header() {
                                     to="/"
                                     onClick={handleMenuToggle} // Close menu on click
                                     className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-700' : 'text-gray-700'}
-                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-600 font-semibold' : 'text-gray-700'}
+                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-600 lg:p-0`
                                     }
                                 >
                                     Home
@@ -70,8 +71,8 @@ export default function Header() {
                                     to="/about"
                                     onClick={handleMenuToggle} // Close menu on click
                                     className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-700' : 'text-gray-700'}
-                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-600 font-semibold' : 'text-gray-700'}
+                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-600 lg:p-0`
                                     }
                                 >
                                     About
@@ -83,8 +84,8 @@ export default function Header() {
                                     to="/contact"
                                     onClick={handleMenuToggle} // Close menu on click
                                     className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-700' : 'text-gray-700'}
-                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-600 font-semibold' : 'text-gray-700'}
+                                    lg:hover:bg-transparent lg:border-0 hover:text-orange-600 lg:p-0`
                                     }
                                 >
                                     Contact
@@ -97,7 +98,7 @@ export default function Header() {
                             <NavLink
                                 to="/login"
                                 className={({ isActive }) =>
-                                    `block text-gray-700   focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 
+                                    `block text-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 
             ${isActive ? 'text-orange-600' : ''}`
                                 }
                                 onClick={handleMenuToggle} // Close menu on click
@@ -108,13 +109,12 @@ export default function Header() {
                             {/* Get Started Button */}
                             <Link
                                 to="#"
-                                className="block mt-4 text-white bg-orange-700  hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-3"
+                                className="block mt-4 text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-3"
                                 onClick={handleMenuToggle} // Close menu on click
                             >
                                 Get started
                             </Link>
                         </div>
-
                     </div>
                 </div>
             </nav>
